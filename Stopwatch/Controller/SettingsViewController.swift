d54a096a2f7d3e5f0624445bf8da04293b48d3ab
Marco <ocram195@hotmail.com>
2021-07-21 21:57:55 +0200

    animations

--- Stopwatch/Controller/SettingsViewController.swift
+++ Stopwatch/Controller/SettingsViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class SettingsViewController: UIViewController, UITextFieldDelegate {
 
+    
     @IBOutlet weak var startOutlet: UIButton!
     @IBOutlet weak var distanceTxtField: UITextField!
-
+    @IBOutlet weak var buttonBottomConstraint: NSLayoutConstraint!
+    
     
     private let accessoryToolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
     private var accessoryDoneButton: UIBarButtonItem!
@@ -27,9 +29,33 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
 
         distanceTxtField.becomeFirstResponder()
         
-      
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        startOutlet.isUserInteractionEnabled = false
+        startOutlet.alpha = 0.5
     }
 
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+          let userInfo = notification.userInfo
+        let keyboardSize = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+          let keyboardHeight = keyboardSize.cgRectValue.height
+        UIView.animate(withDuration: 0.1) {
+            self.buttonBottomConstraint.constant = keyboardHeight-60
+            self.view.layoutIfNeeded()
+        }
+      }
+
+    
+      @objc private func keyboardWillHide(_ notification: Notification) {
+    
+        UIView.animate(withDuration: 0.1) {
+            self.buttonBottomConstraint.constant = 20
+            self.view.layoutIfNeeded()
+        }
+      }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -81,11 +107,25 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
        
         let currentText = textField.text ?? ""
-
+        
         guard let stringRange = Range(range, in: currentText) else { return false }
-
+        
         let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-
+        
+        if updatedText.count > 0 {
+            
+            UIView.animate(withDuration: 0.15) {
+                self.startOutlet.isUserInteractionEnabled = true
+                self.startOutlet.alpha = 1
+            }
+        } else {
+            UIView.animate(withDuration: 0.15) {
+                self.startOutlet.isUserInteractionEnabled = false
+                self.startOutlet.alpha = 0.5
+            }
+            
+        }
+        
         distance = updatedText
         
         // I DECIDED TO LIMIT DISTANCE DIGITS TO 4
