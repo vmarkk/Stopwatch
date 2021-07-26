550382d9d2eefc68c2ba627c22856dc4bf7d7a16
Marco <ocram195@hotmail.com>
2021-07-26 08:33:50 +0200

    graph view

--- Stopwatch/Model/CustomShapes/Chart.swift
+++ Stopwatch/Model/CustomShapes/Chart.swift
@@ -41,17 +41,13 @@ class Chart: UIView {
    
     
     func addPointToGraph() {
-       // circlePath.removeAllPoints()
-       
+
         path.removeAllPoints()
         setNeedsDisplay()
        
-      
-        
         var currentIndex = 0
         for point in  points {
             
-           
             currentIndex += 1
             
             path.move(to: CGPoint(x: lastPoint.x*CGFloat(currentIndex-1), y: lastPoint.y))
@@ -61,33 +57,10 @@ class Chart: UIView {
             lastPoint.x = frame.size.width/CGFloat(points.count)
             
             lastPoint.y = frame.height-point.y
-            
-            // ADD PIN
-          
-            var circlePath = UIBezierPath()
-            let circleLayer = CAShapeLayer()
-      
-            circlePath.cgPath = UIBezierPath(ovalIn: CGRect(x: frame.size.width/CGFloat(points.count)*CGFloat(currentIndex)-4, y: frame.size.height-point.y-4, width: 8, height: 8)).cgPath
-            circleLayer.path = circlePath.cgPath
-         
-            layer.addSublayer(circleLayer)
-            circlePath.removeAllPoints()
-            
-            
-            // ADD PIN
-            
+       
         }
         
-        
-       
-        
         shapeLayer.path = path.cgPath
-      
-      
-      
-        
-       // circleLayer.setNeedsDisplay()
-      
         shapeLayer.setNeedsDisplay()
         
         layer.addSublayer(shapeLayer)
